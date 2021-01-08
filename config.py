cfde5cfb793dfa11d65173fab5f07f0984cfb5b5
Lyor Itzhaki <lyori6ux@gmail.com>
2021-01-07 20:45:32 -0800

    Pagination and blog posts submission

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['lyori6us@gmail.com']
+    POSTS_PER_PAGE = 3
